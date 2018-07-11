341522a2cce99691f406840f7a8e05521d2d658b
Jeremy Magland <jeremy.magland@gmail.com>
2018-07-11 13:18:59 -0400

    0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mountainlab_pytools",
-    version="0.2.1",
+    version="0.2.2",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Tools for using MountainLab with python",

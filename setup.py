216911f32648e923df8bf1d682194a19660cb403
Jeremy Magland <jeremy.magland@gmail.com>
2018-07-11 13:26:30 -0400

    remove long description

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="mountainlab_pytools",
-    version="0.2.2",
+    version="0.2.3",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Tools for using MountainLab with python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/magland/mountainlab_pytools",
     packages=setuptools.find_packages(),
     install_requires=[

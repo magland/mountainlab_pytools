7c4c2bf0b658f75605aaeb63e406112b12316c5a
Jeremy Magland <jeremy.magland@gmail.com>
2018-08-08 14:42:18 -0400

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mountainlab_pytools",
-    version="0.5.2",
+    version="0.6.0",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Tools for using MountainLab with python",

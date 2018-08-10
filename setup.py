a68c907820eccaab4c2e214518e4340a30038bbd
Jeremy Magland <jeremy.magland@gmail.com>
2018-08-10 12:37:34 -0400

    conda deploy

--- setup.py
+++ setup.py
@@ -25,4 +25,31 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ),
+    conda={
+        "build_number":0,
+        "build_script":[
+            "python -m pip install vdom jp_proxy_widget",
+            "python -m pip install --no-deps --ignore-installed .",
+            "CMD=\"ln -sf $SP_DIR/"+pkg_name+" `CONDA_PREFIX=$PREFIX ml-config package_directory`/"+pkg_name+"\"",
+            "echo $CMD",
+            "$CMD"
+        ],
+        "test_commands":[
+        ],
+        "test_imports":[
+            "mountainlab_pytools",
+            "mountainlab_pytools.mlproc",
+            "mountainlab_pytools.mdaio",
+            "mountainlab_pytools.processormanager"
+        ],
+        "requirements":[
+            "python",
+            "pip",
+            "requests",
+            "numpy",
+            "numpydoc",
+            "ipython",
+            "ipywidgets"
+        ]
+    }
 )

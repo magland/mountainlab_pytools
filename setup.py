6fd3173a31b9a7858c64b05169f1f397b71f5eb5
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-13 11:45:27 -0400

    incr version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name="mountainlab_pytools"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.6.10",
+    version="0.6.11",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Tools for using MountainLab with python",

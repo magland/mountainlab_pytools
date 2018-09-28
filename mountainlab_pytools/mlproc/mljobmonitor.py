7d765e9af8be119a0fd8667d117ce8a5593486b5
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-28 09:09:00 -0400

    remove ipython dependency

--- mountainlab_pytools/mlproc/mljobmonitor.py
+++ mountainlab_pytools/mlproc/mljobmonitor.py
@@ -1,11 +1,11 @@
-import jp_proxy_widget
-import IPython, time
+import time
 
 import os
 script_path = os.path.dirname(os.path.abspath( __file__ ))
     
 class MLJobMonitor:
     def __init__(self):
+        import jp_proxy_widget
         self._W=jp_proxy_widget.JSProxyWidget()
         self._initialize_widget(self._W)
     def display(self):
@@ -13,9 +13,11 @@ class MLJobMonitor:
     def widget(self):
         return self._W.element
     def renderNow(self):
+        # import IPython
         # See https://github.com/AaronWatters/jp_proxy_widget/issues/2
-        time.sleep(0.1) # It is unclear how long one needs to sleep... e.g., 0.001 does not seem to work
-        IPython.get_ipython().kernel.do_one_iteration()
+        #time.sleep(0.1) # It is unclear how long one needs to sleep... e.g., 0.001 does not seem to work
+        #IPython.get_ipython().kernel.do_one_iteration()
+        pass
     def _initialize_widget(self,W):
         W.check_jquery()
         
